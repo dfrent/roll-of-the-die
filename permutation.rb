8ba472b0768b8ac4a1f21374f9f02bb353c7905d
cordell <cordell.perry@gmail.com>
2017-11-07 21:12:16 -0500

    doubles and permutation

---
+++ permutation.rb
@@ -0,0 +1,12 @@
+# permutate over a 2 ranges of 6
+# output intergers in 2colums need s line break
+# print roll of each dice
+
+
+(1..6).each do |dice1|
+
+(1..6).each do |dice2|
+
+  puts "#{dice1} #{dice2}"
+end
+end
